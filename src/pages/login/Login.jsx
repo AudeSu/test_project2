7a6877bdf43e18fc464ddabf085866670aeedaa5
AudeSu <aude.sustronck@gmail.com>
2024-03-07 13:52:01 +0100

    second commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./login.scss";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate()
+
+  const handleLogin = (e) => {
+    // https://firebase.google.com/docs/auth/web/password-auth
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        navigate("/")
+      })
+      .catch((error) => {
+        setError(true)
+      })
+  }
+
+  return (
+    <div className="login">
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder="email" onChange={e=>setEmail(e.target.value)}/>
+        <input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)}/>
+        <button type="submit">Login</button>
+        {error && <span>Wrong email or password!</span>}
+      </form>
+    </div>
+  )
+}
+
+export default Login
