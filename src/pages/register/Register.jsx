012d511137a3e1f8b88c66af5e9e5d6fe9bb1065
AudeSu <aude.sustronck@gmail.com>
2024-03-07 13:54:36 +0100

    register toegevoegd

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./login.scss";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+
+const Login = () => {
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = (e) => {
+    // https://firebase.google.com/docs/auth/web/password-auth
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        console.log(user)
+        // ...
+      })
+      .catch((error) => {
+        setError(true)
+      });
+  }
+
+  return (
+    <div className="login">
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder="email" onChange={e=>setEmail(e.target.value)}/>
+        <input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)}/>
+        <button type="submit">Login</button>
+        {error && <span>Wrong email or password!</span>}
+      </form>
+    </div>
+  )
+}
+
+export default Login
